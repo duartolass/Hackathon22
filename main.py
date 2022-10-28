53f9498f4a79a753c42495f27414f005005c7b84
Duarte Santos <duarte_guilhermedossantos@sluz.ch>
2022-10-28 14:24:49 +0200

    battlesnake head and body

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "duartolass",  # Battlesnake Username
+        "color": "#FF8A29",  # Battlesnake Color
+        "head": "nr-rocket",  # TODO: Choose head
+        "tail": "nr-booster",  # TODO: Choose tail
     }
 
 
