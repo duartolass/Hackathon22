a8d37e519608a0f9748cd7204df9b1cec8a83006
Duarte Santos <duarte_guilhermedossantos@sluz.ch>
2022-10-29 03:30:47 +0200

    5.7

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
         next_move = random.choice(safe_moves)
 
     # Check if the next move is risky from other opponents
-    opponents = ["snakes"]
+    opponents = ['snakes']
     for opponent in opponents[1:]:
         for Opponenthead in opponent['head']:
             tempOpHeadAbove = [Opponenthead["x"], Opponenthead[["y"] + 1]]
