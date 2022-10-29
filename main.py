8f660d0bc34d3179398a63373c98aa5c379b677e
Duarte Santos <duarte_guilhermedossantos@sluz.ch>
2022-10-29 03:47:09 +0200

    5.12

--- main.py
+++ main.py
@@ -148,10 +148,10 @@ def move(game_state: typing.Dict) -> typing.Dict:
     opponents = ['snakes']
     for opponent in opponents[1:]:
         for Opponenthead in opponent['head']:
-            tempOpHeadAbove = Opponenthead["x"], Opponenthead["y"] + 1
-            tempOpHeadBelow = Opponenthead["x"], Opponenthead["y"] - 1
-            tempOpHeadRight = Opponenthead["x"] + 1, Opponenthead["y"]
-            tempOpHeadLeft = Opponenthead["x"] - 1, Opponenthead["y"]
+            tempOpHeadAbove = [Opponenthead["x"], Opponenthead["y"] + 1]
+            tempOpHeadBelow = [Opponenthead["x"], Opponenthead["y"] - 1]
+            tempOpHeadRight = [Opponenthead["x"] + 1, Opponenthead["y"]]
+            tempOpHeadLeft = [Opponenthead["x"] - 1, Opponenthead["y"]]
             if next_move_left == tempOpHeadAbove or next_move_left == tempOpHeadBelow or next_move_left == tempOpHeadLeft or next_move_left == tempOpHeadRight: # Opponent next movement is left of head, don't move left
                 is_move_safe["left"] = False
 
